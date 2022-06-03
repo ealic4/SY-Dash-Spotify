d8ba9f6f1c83041a64150ec6104c94509d63c8f0
ealic4 <ealic4@etf.unsa.ba>
2022-06-03 03:19:29 +0200

    Add text box

--- main.py
+++ main.py
@@ -8,15 +8,48 @@ from navbar import navbar
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div(children=[
-#    #html.Div([navbar]),
-   navbar,
- #  glchrt
-#    #html.P('I am a component of the layout as well'),
-    #dcc.Input(id='my=input', placeholder='Enter a value')
-#])
 
+app.layout = html.Div(children=[
+   navbar,
+dbc.Row([dbc.Col([dbc.Row(["Graf1"]),dbc.Row(["Graf2"])]),dbc.Col([dbc.Row([
+
+    dcc.Textarea(
+        id='textarea-example1',
+        value='Textarea content initialized\nwith multiple lines of text',
+        style={'width': '100%', 'height': 100},
+    ),
+    html.Div(id='textarea-example-output1', style={'whiteSpace': 'pre-line'})
+
+
+]),dbc.Row([
+
+html.Div([
+    dcc.Textarea(
+        id='textarea-example2',
+        value='Textarea content initialized\nwith multiple lines of text',
+        style={'width': '100%', 'height': 100, 'margina-top':50},
+    ),
+    html.Div(id='textarea-example-output2', style={'whiteSpace': 'pre-line'})
 ])
+
+
+
+
+
+
+
+
+
+])]),dbc.Col([html.Div([
+    dcc.Textarea(
+        id='textarea-example',
+        value='Textarea content initialized\nwith multiple lines of text',
+        style={'width': '100%', 'height': 300},
+    ),
+    html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
+])])])
+])
+
 
 if __name__ == '__main__':
     app.run_server(port=8080, debug=True)
